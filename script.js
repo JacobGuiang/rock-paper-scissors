a26adc73864c1b78cf90be63870e74727d011413
JacobGuiang <jacob.guiang1@gmail.com>
2022-06-29 12:22:35 -0500

    Add UI to website
    Javascript and DOM manipulation was added for the website UI. The user
    now plays rock paper scissors through the website UI instead of through
    the console.

--- script.js
+++ script.js
@@ -1,68 +1,59 @@
+let playerWins = 0, computerWins = 0;
+
+// computer chooses a random choice between Rock, Paper, or Scissors
 function computerPlay() {
-  const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+  const choices = ['Rock', 'Paper', 'Scissors'];
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 }
 
-function playRound(playerSelection, computerSelection) {
+// play one round of rock paper scissors
+function playRound(playerSelection) {
+  let computerSelection = computerPlay();
+
+  const result = document.querySelector('#result');
+  const playerScore = document.querySelector('#player-score');
+  const computerScore = document.querySelector('#computer-score');
+
   if(playerSelection === computerSelection) {
-    console.log('Draw! You and the computer both chose ' + playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase());
-    return -1;
+    result.textContent = `Draw! You and the computer both chose ${playerSelection}`;
   }
-  else if(playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-    console.log('You Lose! Paper beats Rock');
-    return 0;
+  else if(playerSelection === 'Rock' && computerSelection === 'Paper') {
+    result.textContent = 'You Lose! Paper beats Rock';
+    computerScore.textContent = `Computer: ${++computerWins}`;
   }
-  else if(playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-    console.log('You Win! Rock beats Scissors');
-    return 1;
+  else if(playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    result.textContent = 'You Win! Rock beats Scissors';
+    playerScore.textContent = `Player: ${++playerWins}`;
   }
-  else if(playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-    console.log('You Lose! Rock beats Scissors');
-    return 0;
+  else if(playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    result.textContent = 'You Lose! Rock beats Scissors';
+    computerScore.textContent = `Computer: ${++computerWins}`;
   }
-  else if(playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-    console.log('You Win! Scissors beats Paper');
-    return 1;
+  else if(playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    result.textContent = 'You Win! Scissors beats Paper';
+    playerScore.textContent = `Player: ${++playerWins}`;
   }
-  else if(playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-    console.log('You Lose! Scissors beat Paper');
-    return 0;
+  else if(playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    result.textContent = 'You Lose! Scissors beat Paper';
+    computerScore.textContent = `Computer: ${++computerWins}`;
   }
-  else if(playerSelection === 'PAPER' && computerSelection === 'ROCK') {
-    console.log('You Win! Paper beats Rock');
-    return 1;
+  else if(playerSelection === 'Paper' && computerSelection === 'Rock') {
+    result.textContent = 'You Win! Paper beats Rock';
+    playerScore.textContent = `Player: ${++playerWins}`;
   }
 }
 
-function game() {
-  let playerWins = 0, computerWins = 0;
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    if(playerWins < 5 && computerWins < 5) {
+      playRound(button.id);
 
-  for(let i=0; i<5; ++i) {
-    let playerSelection = prompt('Enter a choice: rock, paper, or scissors').toUpperCase();
-
-    if(playerSelection === 'SCISSOR')
-      playerSelection = 'SCISSORS';
-
-    while(playerSelection !== 'ROCK' && playerSelection !== 'PAPER' && playerSelection !== 'SCISSORS') {
-      playerSelection = prompt('Invalid choice. Enter rock, paper, or scissors').toUpperCase();
-      if(playerSelection === 'SCISSOR')
-        playerSelection = 'SCISSORS';
+      if(playerWins == 5 || computerWins == 5) {
+        const winner = document.querySelector('#winner');
+        winner.textContent = `${(playerWins == 5) ? 'You won' : 'You lost'} the game!`;
+      }
     }
-
-    let computerSelection = computerPlay();
-    let roundResult = playRound(playerSelection, computerSelection);
-    
-    if(roundResult === 0)
-      ++computerWins;
-    else if(roundResult === 1)
-      ++playerWins;
-  }
-
-  if(playerWins > computerWins)
-    console.log('Player Wins');
-  else if(computerWins > playerWins)
-    console.log('Computer Wins');
-  else
-    console.log('Draw');
-}
+  });
+});
